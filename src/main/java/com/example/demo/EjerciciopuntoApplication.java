5f2948c76fe7087ef12f3db4fa65f1b656c7f130
jurguentaker13 <jurguentaker13@gmail.com>
2025-02-03 20:44:34 -0600

    Ejemplo FinalBack

--- src/main/java/com/example/demo/EjerciciopuntoApplication.java
+++ src/main/java/com/example/demo/EjerciciopuntoApplication.java
@@ -2,6 +2,9 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @SpringBootApplication
@@ -10,5 +13,17 @@ public class EjerciciopuntoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EjerciciopuntoApplication.class, args);
 	}
-
+@Bean
+ public WebMvcConfigurer corsConfigurer2(){
+     return new WebMvcConfigurer(){
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**")
+                .allowedOrigins( "https://localhost:4200")
+                .allowCredentials(true)
+                .allowedHeaders("Origin", "Authorization", "Content-Type")
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
+    }
+   };
+ }
 }
